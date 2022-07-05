4cf046ffd22ae64efd6f4daed224f79720fd6102
sunjo <sunjo@prismweaver.com>
2022-07-05 17:18:10 +0900

    9.7 Serverless Sessions

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -35,12 +35,12 @@ async function handler(
         },
     });
     if (phone) {
-        const message = await twilioClient.messages.create({
-            messagingServiceSid: process.env.TWILIO_MSID,
-            to: process.env.MY_PHONE!,
-            body: `Your login token is ${payload}.`,
-        });
-        console.log(message);
+        // const message = await twilioClient.messages.create({
+        //     messagingServiceSid: process.env.TWILIO_MSID,
+        //     to: process.env.MY_PHONE!,
+        //     body: `Your login token is ${payload}.`,
+        // });
+        // console.log(message);
     } else if (email) {
         // const email = await mail.send({
         //     from: "nico@nomadcoders.co",
