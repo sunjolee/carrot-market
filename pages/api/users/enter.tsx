84c8e977b006a076baa987d37497aec972203480
sunjo <sunjo@prismweaver.com>
2022-07-05 11:01:24 +0900

    9.5 Sending Email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,11 @@
+// import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import {PrismaClient} from "@prisma/client";
 
+// mail.setApiKey(process.env.SENDGRID_KEY!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
@@ -39,6 +41,15 @@ async function handler(
             body: `Your login token is ${payload}.`,
         });
         console.log(message);
+    } else if (email) {
+        // const email = await mail.send({
+        //     from: "nico@nomadcoders.co",
+        //     to: "nico@nomadcoders.co",
+        //     subject: "Your Carrot Market Verification Email",
+        //     text: `Your token is ${payload}`,
+        //     html: `<strong>Your token is ${payload}</strong>`,
+        // });
+        // console.log(email);
     }
     return res.json({
         ok: true,
