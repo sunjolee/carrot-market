ee625620aef019360f6226b00bb3ed1f9e81fe53
sunjo <sunjo@prismweaver.com>
2022-07-06 11:54:21 +0900

    10.4 useUser Refactor  :  swr 코드 정리 및 마무리

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,7 @@ import useUser from "@libs/client/useUser";
 import Head from "next/head";
 
 const Home: NextPage = () => {
-    // const { user, isLoading } = useUser();
-    const user = useUser();
+    const { user, isLoading } = useUser();
     console.log(user);
     return (
         <Layout title="홈" hasTabBar>
