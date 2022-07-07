e07fabea2a78f5eb70a3ea79821bcc4a29f99dae
sunjo <sunjo@prismweaver.com>
2022-07-07 23:41:18 +0900

    11.5 Edit Profile part One

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,14 +1,42 @@
-import type {NextPage} from "next";
-import Button from "../../components/button";
-import Input from "../../components/input";
-import Layout from "../../components/layout";
+import type { NextPage } from "next";
+import Button from "@components/button";
+import Input from "@components/input";
+import Layout from "@components/layout";
+import useUser from "@libs/client/useUser";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+
+interface EditProfileForm {
+    email?: string;
+    phone?: string;
+    formErrors?: string;
+}
 
 const EditProfile: NextPage = () => {
+    const { user } = useUser();
+    const {
+        register,
+        setValue,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm<EditProfileForm>();
+    useEffect(() => {
+        if (user?.email) setValue("email", user.email);
+        if (user?.phone) setValue("phone", user.phone);
+    }, [user, setValue]);
+    const onValid = ({ email, phone }: EditProfileForm) => {
+        if (email === "" && phone === "") {
+            setError("formErrors", {
+                message: "Email OR Phone number are required. You need to choose one.",
+            });
+        }
+    };
     return (
         <Layout canGoBack title="Edit Profile">
-            <form className="py-10 px-4 space-y-4">
+            <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
                 <div className="flex items-center space-x-3">
-                    <div className="w-14 h-14 rounded-full bg-slate-500"/>
+                    <div className="w-14 h-14 rounded-full bg-slate-500" />
                     <label
                         htmlFor="picture"
                         className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
@@ -22,15 +50,27 @@ const EditProfile: NextPage = () => {
                         />
                     </label>
                 </div>
-                <Input required label="Email address" name="email" type="email"/>
                 <Input
-                    required
+                    register={register("email")}
+                    required={false}
+                    label="Email address"
+                    name="email"
+                    type="email"
+                />
+                <Input
+                    register={register("phone")}
+                    required={false}
                     label="Phone number"
                     name="phone"
-                    type="number"
+                    type="text"
                     kind="phone"
                 />
-                <Button text="Update profile"/>
+                {errors.formErrors ? (
+                    <span className="my-2 text-red-500 font-medium text-center block">
+            {errors.formErrors.message}
+          </span>
+                ) : null}
+                <Button text="Update profile" />
             </form>
         </Layout>
     );
