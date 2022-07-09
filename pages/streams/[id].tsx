19ceea22583cb92114d0aca363f050a758f28f8f
sunjo <sunjo@prismweaver.com>
2022-07-09 13:00:54 +0900

    14.2 Send Message : 채팅에서 문 보내기

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -4,17 +4,32 @@ import Message from "@components/message";
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import { Stream } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
 
 interface StreamResponse {
     ok: true;
     stream: Stream;
 }
 
+interface MessageForm {
+    message: string;
+}
+
 const Stream: NextPage = () => {
     const router = useRouter();
+    const { register, handleSubmit, reset } = useForm<MessageForm>();
     const { data } = useSWR<StreamResponse>(
         router.query.id ? `/api/streams/${router.query.id}` : null
     );
+    const [sendMessage, { loading, data: sendMessageData }] = useMutation(
+        `/api/streams/${router.query.id}/messages`
+    );
+    const onValid = (form: MessageForm) => {
+        if (loading) return;
+        reset();
+        sendMessage(form);
+    };
     return (
         <Layout canGoBack>
             <div className="py-10 px-4  space-y-4">
@@ -36,17 +51,21 @@ const Stream: NextPage = () => {
                         <Message message="미쳤어" />
                     </div>
                     <div className="fixed py-2 bg-white  bottom-0 inset-x-0">
-                        <div className="flex relative max-w-md items-center  w-full mx-auto">
+                        <form
+                            onSubmit={handleSubmit(onValid)}
+                            className="flex relative max-w-md items-center  w-full mx-auto"
+                        >
                             <input
                                 type="text"
-                                className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
+                                {...register("message", { required: true })}
+                                className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-1 pr-12 focus:border-orange-500"
                             />
                             <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
                                 <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">
                                     &rarr;
                                 </button>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
