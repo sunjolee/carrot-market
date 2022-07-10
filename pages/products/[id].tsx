701c365c99ff49b64829332df5de5d4d37511219
sunjo <sunjo@prismweaver.com>
2022-07-11 07:22:42 +0900

    19.5 Script Component - 외부 스크립트 로딩 시점 구현 및 seo 타이틀 설정

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -48,7 +48,7 @@ const ItemDetail: NextPage = () => {
 
     };
     return (
-        <Layout canGoBack>
+        <Layout canGoBack seoTitle="Product Detail">
             <div className="px-4  py-4">
                 <div className="mb-8">
                     <div className="h-96 bg-slate-300" />
